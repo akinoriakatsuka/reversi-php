f3b13dba995f165f930c6ffbde4c9e5fd7d61c59
akinoriakatsuka <akinoriakatsuka@gmail.com>
2023-10-29 01:39:03 +0900

    add toggle method

--- src/Othello/Color.php
+++ src/Othello/Color.php
@@ -5,4 +5,11 @@ namespace Reversi;
 enum Color: int {
     case Black = 1;
     case White = 2;
+
+    public function toggle(): Color {
+        return match ($this) {
+            Color::Black => Color::White,
+            Color::White => Color::Black,
+        };
+    }
 }
